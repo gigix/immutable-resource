27780939e679e624826b8d288fd4b5b19fa61701
Jeff Xiong <gigix1980@gmail.com>
2014-01-30 14:47:44 +0800

    created a memory-based storage

---
+++ js/storage/memory.js
@@ -0,0 +1,47 @@
+var data = {};
+
+exports.clear = function (callback) {
+    data = {};
+    callback();
+};
+
+exports.create = function (entityType, entity, callback) {
+    entity.id = require('node-uuid').v4();
+    exports.all(entityType, function (error, allEntities) {
+        allEntities.push(entity);
+        callback(null, entity);
+    });
+};
+
+exports.all = function (entityType, callback) {
+    if (data[entityType] == undefined) {
+        data[entityType] = [];
+    }
+    callback(null, data[entityType]);
+};
+
+exports.get = function (entityType, id, callback) {
+    exports.findByField(entityType, {'id': id}, function (error, foundEntities) {
+        callback(error, foundEntities[0]);
+    });
+};
+
+exports.findByField = function (entityType, fieldNameAndValue, callback) {
+    var fieldName = Object.keys(fieldNameAndValue)[0];
+    var fieldValue = fieldNameAndValue[fieldName];
+
+    exports.all(entityType, function (error, allEntities) {
+        var foundEntities = [];
+        allEntities.forEach(function (entity) {
+            if (entity[fieldName] == fieldValue) {
+                foundEntities.push(entity);
+            }
+        });
+        callback(null, foundEntities);
+    });
+};
+
+exports.find = function (entityType, criteria, callback) {
+    // implemented as findByField
+    exports.findByField(entityType, criteria, callback);
+};
--- spec/repository-spec.js
+++ spec/repository-spec.js
@@ -1,48 +1,58 @@
 describe('Repository', function () {
-    var Repository = require('../js/repository');
-    var esStorage = require('../js/es');
-    var patientRepository = new Repository(esStorage, 'patient');
-
-    beforeEach(function (done) {
-        patientRepository.clear(done);
-        patientRepository.all(function (error, resources) {
-            expect(resources.length).toEqual(0);
-            done();
-        });
+    ['memory', 'es'].forEach(function (storageName) {
+        testRepositoryUponStorage(storageName);
     });
+});
 
-    describe('create', function () {
-        it('should create a new resource', function (done) {
-            var newPatient = {
-                firstName: 'Jeff',
-                lastName: 'Xiong'
-            };
-            patientRepository.create(newPatient, function (error, persistedPatientId) {
-                expect(error).toEqual(null);
-                patientRepository.load(persistedPatientId, function (error, loadedPatient) {
-                    expect(loadedPatient.firstName).toEqual('Jeff');
-                    done();
+function testRepositoryUponStorage(storageName) {
+    var storage = require('../js/storage/' + storageName);
+
+    describe('upon [' + storageName + '] storage', function () {
+        var Repository = require('../js/repository');
+        var patientRepository = new Repository(storage, 'patient');
+
+        beforeEach(function (done) {
+            patientRepository.clear(done);
+            patientRepository.all(function (error, resources) {
+                expect(resources.length).toEqual(0);
+                done();
+            });
+        });
+
+        describe('create', function () {
+            it('should create a new resource', function (done) {
+                var newPatient = {
+                    firstName: 'Jeff',
+                    lastName: 'Xiong'
+                };
+                patientRepository.create(newPatient, function (error, persistedPatientId) {
+                    expect(error).toEqual(null);
+                    expect(persistedPatientId).not.toEqual(undefined);
+                    patientRepository.load(persistedPatientId, function (error, loadedPatient) {
+                        expect(loadedPatient.firstName).toEqual('Jeff');
+                        done();
+                    });
                 });
             });
         });
-    });
 
-    describe('update', function () {
-        it('should update final state of resource', function (done) {
-            var newPatient = {
-                firstName: 'Jie',
-                lastName: 'Xiong'
-            };
-            patientRepository.create(newPatient, function (error, persistedPatientId) {
-                patientRepository.load(persistedPatientId, function (error, loadedPatient) {
-                    patientRepository.update(loadedPatient.id, {firstName: 'Jeff'}, function (error) {
-                        patientRepository.load(persistedPatientId, function (error, updatedPatient) {
-                            expect(updatedPatient.firstName).toEqual('Jeff');
-                            done();
+        describe('update', function () {
+            it('should update final state of resource', function (done) {
+                var newPatient = {
+                    firstName: 'Jie',
+                    lastName: 'Xiong'
+                };
+                patientRepository.create(newPatient, function (error, persistedPatientId) {
+                    patientRepository.load(persistedPatientId, function (error, loadedPatient) {
+                        patientRepository.update(loadedPatient.id, {firstName: 'Jeff'}, function (error) {
+                            patientRepository.load(persistedPatientId, function (error, updatedPatient) {
+                                expect(updatedPatient.firstName).toEqual('Jeff');
+                                done();
+                            });
                         });
                     });
                 });
             });
         });
     });
-});
+}
